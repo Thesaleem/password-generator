e4536624517d402c223b0f1d2198817d5b49325b
Thesaleem <89635859+Thesaleem@users.noreply.github.com>
2022-08-16 17:37:56 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,65 @@
+# Personal Project - Password Generator
+
+This is a personal project on building a password generator which is at least 12 characters long.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- Generate passwords containing upper and lower case characters, numbers and special characters
+
+
+### Links
+
+- Live Site URL: [Password Generator](https://p-wordgen.netlify.app)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Tailwind CSS
+- Flexbox
+- Mobile-first workflow
+- Vanilla Javascript
+
+
+### What I learned
+
+Learnt how to use Tailwind CSS while building this project.
+
+
+### Continued development
+
+Build more with Tailwind Css.
+
+### Useful resources
+
+-Tailwind CSS Documentation.
+
+
+## Author
+
+- Twitter - [@TheSaleem_](https://www.twitter.com/thesaleem_)
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict'
+const inputEl = document.querySelector('#password-text')
+const copyBtn = document.querySelector('.copy-btn')
+const generateBtn = document.querySelector('.generate-btn')
+const alphabetLc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const alphabetUc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+const specialCharac = [`!`, `"`, `#`,`$`, `%`, `&`, `'`, `(`, `)`, `*`, `+`, `,`, `-`, `.`, `/`, `:`, `;`, `=`, `?`, `@`, `[`, `]`, `^`, `{`, `|`, `}`, `~`]
+const allChar = [...alphabetLc,...alphabetUc,...numbers,...specialCharac]
+const arr = ["s", 'a','l',"e" ,"e", "m" ]
+let pass = '';
+generateBtn.addEventListener('click', function(e){
+    e.preventDefault()
+    if(!(inputEl.value === "")){
+        pass = ''
+        inputEl.value = ''
+    }
+    if(inputEl.value === ""){
+        for(let i = 0; i < 12; i++){
+            let randnum = Math.floor(Math.random()* allChar.length);
+            pass += allChar[randnum]
+        }
+        inputEl.value = pass;  
+    }
+})
+copyBtn.addEventListener('click', function(e){
+    e.preventDefault()
+    navigator.clipboard.writeText(inputEl.value).then(() => {
+        console.log('copied');
+    }, () => {
+        console.log('failed');
+    })
+})
+
+
+
